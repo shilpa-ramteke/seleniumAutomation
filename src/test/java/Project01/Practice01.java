a6388cc7cdd8c74c921fb9b904ca89f5a8a1793d
shilpabor <shilpaborkar294@gamil.com>
2024-10-13 16:11:30 +0530

    Add all files

--- src/test/java/Project01/Practice01.java
+++ src/test/java/Project01/Practice01.java
@@ -1,2 +1,17 @@
-package Project01;public class Practice01 {
+package Project01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Practice01 {
+
+    @Test
+    public void Test01()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://sdet.live");
+        driver.quit();
+
+    }
 }
